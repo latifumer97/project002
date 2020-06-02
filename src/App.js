61eb2d47414f29eddfbade3032493ce884160d42
SM UmerLatif <39081061+latifumer97@users.noreply.github.com>
2020-06-03 00:28:55 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Assignment#2 created react app and eployed it on surge by SM UmerLatif
         </p>
         <a
           className="App-link"
