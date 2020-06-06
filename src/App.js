f1d9a8b1663fc801d481e601ea75f1f2ae615ea2
SM UmerLatif <39081061+latifumer97@users.noreply.github.com>
2020-06-06 23:20:56 +0500

    update

--- src/App.js
+++ src/App.js
@@ -2,25 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Assignment#2 created react app and eployed it on surge by SM UmerLatif
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(props) {
+  return <div>
+    hello world from APP.js <br></br>
+    Name= {props.name}<br></br>
+    Age= {props.age}
+    <div>div inside a div: a tree</div>
     </div>
-  );
 }
 
 export default App;
